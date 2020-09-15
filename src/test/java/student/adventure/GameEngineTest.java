e871f044e400733e3b6950e5dd7ba8afd1b8ce8a
annabellej <annabelleju02@gmail.com>
2020-09-14 19:08:57 -0700

    Wrote GameEngine tests using simulated inputs and outputs by player.

--- src/test/java/student/adventure/GameEngineTest.java
+++ src/test/java/student/adventure/GameEngineTest.java
@@ -4,37 +4,239 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.io.*;
 
+import org.hamcrest.*;
+
 public class GameEngineTest {
-    GameEngine testGame;
+    GameEngine gamePlayTester;
+    InputStream inputStream;
+    ByteArrayOutputStream outputStream;
+    String playerInput;
 
     @Before
     public void setUp() throws IOException {
-        testGame = new GameEngine("src/test/resources/fullValidGame.json");
+        outputStream = new ByteArrayOutputStream();
+        gamePlayTester = new GameEngine("src/test/resources/fullValidGame.json", outputStream);
     }
 
     //Tests for correct initial game settings
     @Test
     public void testEmptyInitialInventory() {
-        assertEquals(0, testGame.getPlayerInventory().size());
+        assertEquals(0, gamePlayTester.getPlayerInventory().size());
     }
 
     @Test
     public void testGameNotEnded() {
-        assertTrue(!testGame.isGameEnded());
+        assertFalse(gamePlayTester.isGameEnded());
     }
 
     @Test
     public void testInitialRoomNumberIsOne() {
-        assertEquals(1, testGame.getCurrentRoom().getRoomNumber());
+        assertEquals(1, gamePlayTester.getCurrentRoom().getRoomNumber());
     }
 
     //Tests for moving around:
+    @Test
+    public void testValidGoCommand() {
+        playerInput = "go east" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You have moved to: Hallway"));
+        assertEquals("Hallway", gamePlayTester.getCurrentRoom().getRoomName());
+    }
+
+    @Test
+    public void testInvalidGoCommand() {
+        playerInput = "go north" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("I can't go north."));
+    }
+
+    //Test quitting the game:
+    @Test
+    public void testQuittingGame() {
+        playerInput = "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("Thanks for playing!"));
+    }
+
+    @Test
+    public void testExitingGame() {
+        playerInput = "exit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("Thanks for playing!"));
+    }
 
     //Tests for interacting with items:
+    @Test
+    public void testTakingValidItem() {
+        playerInput = "take chair" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You have picked up: chair."));
+        assertTrue(gamePlayTester.getPlayerInventory().contains("chair"));
+    }
+
+    @Test
+    public void testTakingItemAlreadyInInventory() {
+        playerInput = "take chair" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.addToInventory("chair");
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You already have chair!"));
+    }
+
+    @Test
+    public void testTakingItemNotInRoom() {
+        playerInput = "take key" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("There is no key in the room."));
+    }
+
+    @Test
+    public void testDroppingValidItem() {
+        playerInput = "drop potato" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.addToInventory("potato");
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You've dropped: potato."));
+        assertEquals(-1, gamePlayTester.getPlayerInventory().indexOf("potato"));
+        assertTrue(gamePlayTester.getCurrentRoom().getItemsVisible().contains("potato"));
+    }
+
+    @Test
+    public void testDroppingItemAlreadyInRoom() {
+        playerInput = "drop chair" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.addToInventory("chair");
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("The item chair is already in this room!"));
+        assertEquals(-1, gamePlayTester.getPlayerInventory().indexOf("chair"));
+    }
+
+    @Test
+    public void testDroppingItemNotInInventory() {
+        playerInput = "drop chair" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You don't have chair!"));
+    }
 
     //Test examining room:
+    @Test
+    public void testExamineCommand() {
+        playerInput = "examine" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
 
-    //Test quitting game:
+        gamePlayTester.runGame(inputStream);
 
-    //Test winning game:
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("Examining this room..."));
+    }
+
+    //Test reaching end room:
+    @Test
+    public void testWinningGame() {
+        //Player must have reached the end room AND have the key
+        playerInput = "go south" + "\n" + "take key" + "\n" + "go north" + "\n" +
+                      "go east" + "\n" + "go east" + "\n" + "go south" + "\n" +
+                      "go south" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("Congrats!"));
+        assertTrue(gamePlayTester.isGameEnded());
+    }
+
+    @Test
+    public void testEndRoomMissingKey() {
+        playerInput = "go east" + "\n" + "go east" + "\n" + "go south" + "\n" + "go south" + "\n"
+                    + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You seem to be missing a key..."));
+    }
+
+    //Test player command syntax issues: (casing/white space, unknown command, other command names)
+    @Test
+    public void testWhiteSpaceInCommand() {
+        playerInput = "go                      east" + "\n" + "         quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You have moved to: Hallway"));
+    }
+
+    @Test
+    public void testCasingIssuesInCommand() {
+        playerInput = "gO eaSt" + "\n" + "QUIT" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You have moved to: Hallway"));
+    }
+
+    @Test
+    public void testUnknownCommand() {
+        playerInput = "jump" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("I don't understand jump."));
+    }
+
+    @Test
+    public void testAlternativeCommandName() {
+        playerInput = "move south" + "\n" + "quit" + "\n";
+        inputStream = new ByteArrayInputStream(playerInput.getBytes());
+
+        gamePlayTester.runGame(inputStream);
+
+        String gameOutput = outputStream.toString();
+        assertThat(gameOutput, CoreMatchers.containsString("You have moved to: Storage Closet"));
+    }
 }
