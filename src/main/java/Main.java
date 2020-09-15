8dcb10c8a080aa9077ab752ebdd0b2b302785399
annabellej <annabelleju02@gmail.com>
2020-09-14 19:13:31 -0700

    Added javadocs to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import student.adventure.GameEngine;
 import java.io.IOException;
 
 public class Main {
+    /**
+     * Runs the game Kidnapped!
+     */
     public static void main(String[] args) throws IOException {
         GameEngine newGame = new GameEngine("src/test/resources/fullValidGame.json");
         newGame.runGame();
